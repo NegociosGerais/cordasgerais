d41215d85563bc27b3f62345db5f85c7c6c850b9
Italo Albuquerque <120332994+NegociosGerais@users.noreply.github.com>
2022-12-17 20:54:49 -0300

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+var swiper = new Swiper(".books-slider", {
+
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
 var swiper = new Swiper(".featured-slider", {
 
     spaceBetween: 10,
